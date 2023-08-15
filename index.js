df874821084f76ec0a72747001f931cbf7c44930
Chaz Coats <chazcoats2@hotmail.com>
2023-08-15 13:16:25 -0400

    Forgot to add files to gitignore file. Whoops

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var inquirer = import("inquirer");
-var jest = require("jest");
+//importing necessary packages 
+var inquirer = require("inquirer");
 var {circle, triangle, square} = require("./lib/shapes");
 var fs = require("fs");
 
+//questions for inquirer to prompt
 const questions = [
     {
         name: "text",
@@ -27,28 +28,35 @@ const questions = [
     }
 ]
 
+//function to write generated svg logo to svg file
 function writeToFile(filename, data) {
 fs.writeFile(filename, data, function(err) {
     if (err) {
         return console.log("error");
     }
+    console.log("generated logo.svg");
 })
 }
 
 async function shapeRender() {
+    //prompts user to define logo characteristics. Returns response data
     inquirer.prompt(questions).then(function(data) {
-        if (data.shape === "circle") {
-            let svgCircle = new circle(response.text, response.textColor, response.shapeColor);
+        //checks to see if logo text fits 3 character max requirement. If it does, checks to see what shape was selected then parses that data into the shape object and writes the rendered object to a logo.svg file
+        if (data.text.length > 3) {
+            console.log("error. Invalid text length");
+        }
+        else if (data.shape === "circle") {
+            let svgCircle = new circle(data.text, data.textColor, data.shapeColor);
             writeToFile("./examples/logo.svg", svgCircle.render())
         }
 
-        if (data.shape === "triangle") {
-            let svgTriangle = new triangle(response.text, response.textColor, response.shapeColor);
+        else if (data.shape === "triangle") {
+            let svgTriangle = new triangle(data.text, data.textColor, data.shapeColor);
             writeToFile("./examples/logo.svg", svgTriangle.render())
         }
 
-        if (data.shape === "square") {
-            let svgSquare = new square(response.text, response.textColor, response.shapeColor);
+        else if (data.shape === "square") {
+            let svgSquare = new square(data.text, data.textColor, data.shapeColor);
             writeToFile("./examples/logo.svg", svgSquare.render())
         }
     });
